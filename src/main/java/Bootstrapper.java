2f9ed7fe491f9b6ee69c647d86c0901d95dd8856
zolkiss <kisszoltan89@gmail.com>
2021-12-01 18:36:11 +0100

    Gradle upgrade - Minor Cleanup

--- src/main/java/Bootstrapper.java
+++ src/main/java/Bootstrapper.java
@@ -1,12 +1,9 @@
 import controller.Server;
-import controller.ConsoleSpammer;
 import model.Variables;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,9 +17,6 @@ public class Bootstrapper {
 
     private final Path mcPalLocationDir;
     private String backupPath;
-    private String maxHeapSize;
-    private String jarName;
-//    private final Path worldName;
     private List<String> additionalCommandsToRunAfterBackup;
 
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -44,10 +38,6 @@ public class Bootstrapper {
         } else {
             throwInvalidStartArgumentsException();
         }
-
-//        worldName = searchWorldName(mcPalLocationDir);
-
-//        checkEula(mcPalLocationDir);
     }
 
     private Path evaluatePathOfJar() throws URISyntaxException {
@@ -67,13 +57,6 @@ public class Bootstrapper {
         MCpal.start();
     }
 
-    private static boolean isOneOfThemNull(Object... objects) {
-        for (Object object : objects) {
-            if (object == null) return true;
-        }
-        return false;
-    }
-
     private static List<String> extractAdditionalArguments(List<String> arguments) {
         return arguments.stream()
                 .filter(a -> a.startsWith(Variables.ADDITIONAL_ARGUMENT_PREFIX))
@@ -87,50 +70,6 @@ public class Bootstrapper {
                 .findFirst()
                 .map(arg -> arg.substring(argumentPrefix.length()))
                 .orElse(null);
-    }
-
-    private static Path searchWorldName(Path fromPath) {
-        try {
-            final DirectoryStream<Path> dirStream = Files.newDirectoryStream(fromPath);
-            for (Path currentElement : dirStream) {
-                if (Files.isDirectory(currentElement) && couldThisDirectoryPossiblyBeTheWorldFolder(currentElement)) {
-                    return currentElement.getFileName();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        new Thread(new ConsoleSpammer(Variables.WORLD_DID_NOT_EXIST)).start();
-        return null;
-    }
-
-    private static void checkEula(Path fromPath) {
-        try {
-            final Path eulaPath = fromPath.resolve("eula.txt");
-            final File eulaFile = eulaPath.toFile();
-            if (Files.exists(eulaPath)) {
-                final List<String> readAllLines = Files.readAllLines(eulaPath);
-                final StringBuilder sb = new StringBuilder();
-                readAllLines.forEach(line -> sb.append(line + System.getProperty("line.separator")));
-                String eula = sb.toString();
-                if (eula.contains("eula=false")) {
-                    eula = eula.replace("eula=false", "eula=true");
-                    FileWriter fw = new FileWriter(eulaFile);
-                    fw.write(eula);
-                    fw.flush();
-                    fw.close();
-                }
-            } else {
-                new Thread(new ConsoleSpammer(Variables.EULA_NOT_FOUND)).start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static boolean couldThisDirectoryPossiblyBeTheWorldFolder(Path currentElement) {
-        Path dim1File = currentElement.resolve("DIM1");
-        return Files.exists(dim1File);
     }
 
     private static void throwInvalidStartArgumentsException() {
